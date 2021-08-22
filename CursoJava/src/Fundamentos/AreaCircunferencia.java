b878b05310a9e8472689659970d890fff52d5d9b
Geovany Cipriano <geocipriano28@gmail.com>
2021-08-22 13:54:56 -0300

    ExercicioTemperatura
    Conversor de temperatura

--- CursoJava/src/Fundamentos/AreaCircunferencia.java
+++ CursoJava/src/Fundamentos/AreaCircunferencia.java
@@ -1,5 +1,11 @@
 package Fundamentos;
 
 public class AreaCircunferencia {
-
+	public static void main(String[] args) {
+		double raio = 3.4;
+		final double PI = 3.14159;
+		double area = PI * raio * raio;
+		
+		System.out.println("Area = " + area + " m².");
+	};
 }
---
+++ CursoJava/src/Fundamentos/Temperatura.java
@@ -0,0 +1,12 @@
+package Fundamentos;
+
+public class Temperatura {
+	public static void main(String[] args) {
+		double fire = 86;
+		final double C1 = 32;
+		final double C2 = 5.0 / 9.0;
+		double cel = (fire - C1) * C2;
+		
+		System.out.println(fire + " Fahrenheit = "+ cel + " Celsius");
+	}
+}
