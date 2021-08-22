4ca71121832abd2fa79cf9dc20d8437fad7dabca
Geovany Cipriano <geocipriano28@gmail.com>
2021-08-22 17:15:17 -0300

    Finalizando Inferencia
    Finalizando Inferencia

--- CursoJava/src/Fundamentos/Inferencia.java
+++ CursoJava/src/Fundamentos/Inferencia.java
@@ -1,5 +1,16 @@
 package Fundamentos;
 
 public class Inferencia {
-
+	public static void main(String[] args) {
+		//É possivel indicar uma variavel pelo tipo
+		double a = 4.5;
+		System.out.println(a);
+		
+		//Tambem é possivel indicar uma variavel, sem especificar seu tipo
+		//O java fara a inferencia, indicando o tipo da variavel de acordo com o
+		//valor que foi passado
+		 
+		var b = 4.5;
+		System.out.println(b);
+	}
 }
