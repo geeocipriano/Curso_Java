0feb9be97eb0140f7b3d6bfb0e05329242c92d0f
Geovany Cipriano <geocipriano28@gmail.com>
2021-09-04 01:26:07 -0300

    Update Exercicio IfElse
    Update

--- CursoJava/src/controle/DesafioIfElse.java
+++ CursoJava/src/controle/DesafioIfElse.java
@@ -2,13 +2,40 @@ package controle;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class DesafioIfElse {
 	public static void main(String[] args) {
-		Scanner entrada = new Scanner(System.in);
+		String valor = JOptionPane.showInputDialog("Informe o numero: ");
+		String dia = valor.toString();
+		Boolean domingo = dia.equals("Domingo");
+		Boolean segunda = dia.equals("Segunda");
+		Boolean terca = dia.equals("Terca");
+		Boolean quarta = dia.equals("Quarta");
+		Boolean quinta = dia.equals("Quinta");
+		Boolean sexta = dia.equals("Sexta");
+		Boolean sabado = dia.equals("Sabado");
 
-		System.out.println("Informe sua nota: ");
-		double dia = entrada.nextDouble();
-		boolean check = dia == String;
-		
+		if (domingo) {
+			System.out.println("1");
+		}
+		if (segunda) {
+			System.out.println("2");
+		}
+		if (terca) {
+			System.out.println("3");
+		}
+		if (quarta) {
+			System.out.println("4");
+		}
+		if (quinta) {
+			System.out.println("5");
+		}
+		if (sexta) {
+			System.out.println("6");
+		}
+		if (sabado) {
+			System.out.println("7");
+		}
 	}
 }
