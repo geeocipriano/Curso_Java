efc1f34e3fa6ccaede3a50a6f76b12036fc8272b
Geovany Cipriano <geocipriano28@gmail.com>
2021-09-07 20:43:52 -0300

    Desafio Data
    Criar classe data com dia mes e ano e criar um objeto a partir dela.

---
+++ CursoJava/src/classe/DataTeste.java
@@ -0,0 +1,12 @@
+package classe;
+
+public class DataTeste {
+	public static void main(String[] args) {
+		Data data = new Data();
+		data.dia = 13;
+		data.mes = 03;
+		data.ano = 1996;
+		
+		System.out.println(data.dia + "/" + data.mes + "/" + data.ano);
+	}
+}
